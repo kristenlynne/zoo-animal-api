3ad97a94c06e2758217f8cfecefd86719df8a198
Kristen Smith <kristennlynne@gmail.com>
2022-08-24 15:47:35 -0700

    updated html and css

--- js/main.js
+++ js/main.js
@@ -7,20 +7,28 @@ x fetch data
 x manipulate data and display in DOM
 x create carousel of random animals
 x refresh page for more random animals!
+- make it so duplicate animals don't show :/
 - style CSS
 */
 
+// Event Listeners
 document.addEventListener('DOMContentLoaded', getZooAnimals); 
 document.querySelector('button').addEventListener('click', getZooAnimals);
+// Refreshes page & fetches more animals
+window.addEventListener("load", event => {
+  document.querySelector('.reloadPage').onclick = function() {
+      location.reload(true);
+  }
+});
 
+// Fetches & iterates through data, creates carousel functionality
 async function getZooAnimals() {
-  const url = `https://zoo-animal-api.herokuapp.com/animals/rand/5`
+  const url = `https://zoo-animal-api.herokuapp.com/animals/rand/10`
 
   try {
     const res = await fetch(url);
     const data = await res.json();
       console.log(data);
-      resetDOM();
       data.forEach(animal => {
         console.log(animal)
         addToDOM(animal)
@@ -45,6 +53,7 @@ async function getZooAnimals() {
   }
 }
 
+// manipulates DOM and gets properties from data
 function addToDOM(animal) {
   const li = document.createElement('li');
   li.classList.add('slide')
@@ -54,27 +63,19 @@ function addToDOM(animal) {
       <h1>${animal.name}</h1>
       <img src="${animal.image_link}" alt="${animal.name}"/>
       <ul>
-        <li>Latin Name: ${animal.latin_name}</li>
-        <li>Animal Type: ${animal.animal_type}</li>
-        <li>Habitat: ${animal.habitat}</li>
-        <li>Location: ${animal.geo_range}</li>
-        <li>Active Time: ${animal.active_time}</li>
-        <li>Diet: ${animal.diet}</li>
-        <li>Lifespan: ${animal.lifespan} years</li>
-        <li>Weight: ${animal.weight_min}lbs - ${animal.weight_max}lbs</li>
-        <li>Length: ${animal.length_min}ft - ${animal.length_max}ft</li>
+        <li><b>Latin Name:</b> ${animal.latin_name}</li>
+        <li><b>Animal Type:</b> ${animal.animal_type}</li>
+        <li><b>Habitat:</b> ${animal.habitat}</li>
+        <li><b>Location:</b> ${animal.geo_range}</li>
+        <li><b>Active Time:</b> ${animal.active_time}</li>
+        <li><b>Diet:</b> ${animal.diet}</li>
+        <li><b>Lifespan:</b> ${animal.lifespan} years</li>
+        <li><b>Weight:</b> ${animal.weight_min} - ${animal.weight_max} lbs</li>
+        <li><b>Length:</b> ${animal.length_min} - ${animal.length_max} ft</li>
       </ul>
     </div>
   `;
   document.querySelector('#slides-container').appendChild(li);
-}
-
-function resetDOM() {
-  window.addEventListener("load", event => {
-    document.querySelector('button').onclick = function() {
-        location.reload(true);
-    }
-  });
 }
 
 /////////
